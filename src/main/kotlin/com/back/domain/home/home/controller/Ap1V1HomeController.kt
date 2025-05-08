54351a714488b5ff9c5c1f99cadee7df43b999bb
jhs512 <jangka512@gmail.com>
2025-05-09 00:37:57 +0900

    work

---
+++ src/main/kotlin/com/back/domain/home/home/controller/Ap1V1HomeController.kt
@@ -0,0 +1,19 @@
+package com.back.domain.home.home.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.net.InetAddress
+
+@RestController
+class Ap1V1HomeController {
+
+    @GetMapping("/")
+    fun main(): Map<String, String> {
+        val inetAddress = InetAddress.getLocalHost()  // 로컬 호스트 정보 가져오기
+
+        return mapOf(
+            "hostname" to inetAddress.hostName,       // 호스트 이름
+            "hostAddress" to inetAddress.hostAddress  // IP 주소
+        )
+    }
+}
